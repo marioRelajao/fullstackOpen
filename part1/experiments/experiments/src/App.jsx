a22659b5491c8e90142049fb5456a46a02692ffc
marioRelajao <mario.cerda@estudiantat.upc.edu>
2024-07-29 01:30:30 +0200

    2.1 Done FINALLY

--- part1/experiments/experiments/src/App.jsx
+++ part1/experiments/experiments/src/App.jsx
@@ -1,31 +1,29 @@
-import { useState } from 'react'
-
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-const Display = props => <div>{props.value}</div>
-
+import Course from './Components/Course'
 
 const App = () => {
-  const [value, setValue] = useState(10)
-
-
-  const setToValue = (newValue) => {
-    console.log('value now', newValue)
-    setValue(newValue)
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
   }
 
-  return (
-    <div>
-      <Display value={value} />
-      <Button handleClick={() => setToValue(1000)} text="thousand" />
-      <Button handleClick={() => setToValue(0)} text="reset" />
-      <Button handleClick={() => setToValue(value + 1)} text="increment" />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
